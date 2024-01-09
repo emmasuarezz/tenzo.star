22c267fba004f82ae7439f71cff6ea28699a4533
Emmanuel <emmanuelsuareztou@gmail.com>
2024-01-09 20:02:38 -0300

    fixing the routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Calculate, Landing, Result } from "./Pages";
 import "./styles/CSS/reset.css";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AstroDataProvider } from "./Context/astroContext"; // Import the context provider
 
 const App = () => (
   <React.StrictMode>
-    <AstroDataProvider>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/calculate" element={<Calculate />} />
-        <Route path="/result" element={<Result />} />
-      </Routes>
-    </AstroDataProvider>
+    <Router>
+      <AstroDataProvider>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/calculate" element={<Calculate />} />
+          <Route path="/result" element={<Result />} />
+        </Routes>
+      </AstroDataProvider>
+    </Router>
   </React.StrictMode>
 );
 

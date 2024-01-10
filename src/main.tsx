70cf31c16bbab2a1853f064548ca293cc731c2a4
Emmanuel <emmanuelsuareztou@gmail.com>
2024-01-10 13:33:57 -0300

    Revert "fixing the routing"
    This reverts commit 22c267fba004f82ae7439f71cff6ea28699a4533.

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Calculate, Landing, Result } from "./Pages";
 import "./styles/CSS/reset.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { AstroDataProvider } from "./Context/astroContext"; // Import the context provider
 
 const App = () => (
   <React.StrictMode>
-    <Router>
-      <AstroDataProvider>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/calculate" element={<Calculate />} />
-          <Route path="/result" element={<Result />} />
-        </Routes>
-      </AstroDataProvider>
-    </Router>
+    <AstroDataProvider>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/calculate" element={<Calculate />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
+    </AstroDataProvider>
   </React.StrictMode>
 );
 
